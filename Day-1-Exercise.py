04b4a2a3d243f4a04c2d0cc901ff4873f8710db5
Ajohnl1 <arthur.j.levitan@hotmail.com>
2023-10-02 16:19:41 -0400

    Day 1 Work

---
+++ Day-1-Exercise.py
@@ -0,0 +1,22 @@
+print("Hello, World! ECHOOOOOO!")
+
+num1 = int(input("Enter a number: "))
+print(num1 + 1)
+
+num2 = float(input("Enter a number: "))
+print(num2 + 0.5)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+print(num1 + num2)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+print(num1 * num2)
+
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+whole_number = num1 // num2
+remainder = num1 % num2
+print(whole_number)
+print(remainder)
